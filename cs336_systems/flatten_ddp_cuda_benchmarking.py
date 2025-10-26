8974ce6f4e0c9358501005fee1a9d0de48465467
zyc <1687436023@qq.com>
2025-10-26 15:16:46 +0800

    change into smaller size model

--- cs336_systems/flatten_ddp_cuda_benchmarking.py
+++ cs336_systems/flatten_ddp_cuda_benchmarking.py
@@ -247,18 +247,18 @@ def main():
         return
     
     # XL model configuration
-    config = ModelConfig()
+    # config = ModelConfig()
 
     #smaller size model
-    # config = ModelConfig(
-    #     vocab_size=50257,
-    #     d_model=768,    # 缩小模型维度
-    #     n_layers=12,     # 缩少层数
-    #     n_heads=12,      # 少些注意力头
-    #     d_ff=3072,       # MLP 隐藏层
-    #     max_seq_len=128, # 缩短序列长度
-    #     dropout=0.1
-    # )
+    config = ModelConfig(
+        vocab_size=50257,
+        d_model=768,    # 缩小模型维度
+        n_layers=12,     # 缩少层数
+        n_heads=12,      # 少些注意力头
+        d_ff=3072,       # MLP 隐藏层
+        max_seq_len=128, # 缩短序列长度
+        dropout=0.1
+    )
     
     # Run benchmark
     mp.set_start_method("spawn", force=True)
